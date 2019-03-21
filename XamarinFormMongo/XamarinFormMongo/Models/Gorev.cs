a55e340785071cc8ff3ac32566edddddc7ab9135
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-21 14:00:04 +0300

    mongo db config ok.

---
+++ XamarinFormMongo/XamarinFormMongo/Models/Gorev.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace XamarinFormMongo.Models
+{
+    public class Gorev
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string GorevAdi { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime BitisTarihi { get; set; }
+        public DateTime EklenmeTarihi { get; set; } = DateTime.Now;
+    }
+}
--- XamarinFormMongo/XamarinFormMongo/Views/MainPage.xaml.cs
+++ XamarinFormMongo/XamarinFormMongo/Views/MainPage.xaml.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinFormMongo.Services;
 
 namespace XamarinFormMongo.Views
 {
@@ -18,6 +21,22 @@ namespace XamarinFormMongo.Views
             MasterBehavior = MasterBehavior.Popover;
 
             MenuPages.Add((int)MenuItemType.Browse, (NavigationPage)Detail);
+            MongoTest();
+        }
+
+        private void MongoTest()
+        {
+            var db = MongoService.Db;
+            var collection = db.GetCollection<Gorev>("gorevler");
+            var models = new WriteModel<Gorev>[1];
+            var gorev = new Gorev()
+            {
+                Aciklama = "Test girişi",
+                BitisTarihi = DateTime.Now.AddDays(10),
+                GorevAdi = "Test görevi"
+            };
+            models[0] = new ReplaceOneModel<Gorev>(new BsonDocument("_id", gorev.Id), gorev) { IsUpsert = true };
+            collection.BulkWrite(models);
         }
 
         public async Task NavigateFromMenu(int id)
